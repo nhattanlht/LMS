191f345614be1f78fdad630c6fc1c4f46db90762
ltdthao <thao452004@gmail.com>
2025-01-01 17:22:44 +0700

    add forum fe

--- server/routes/forum.js
+++ server/routes/forum.js
@@ -13,6 +13,6 @@ router.post('/forums', isAuth, createForum);
 router.post('/forums/question', isAuth, createQuestion);
 router.post('/forums/answer', isAuth, createAnswer);
 router.get('/forums', isAuth, getForums);
-router.get('/forums/:questionId', isAuth, getQuestions);
-router.get('/posts/:threadId', isAuth, getAnswers);
+router.get('/forums/:forumId', isAuth, getQuestions);
+router.get('/posts/:forumId/:questionId', isAuth, getAnswers);
 export default router;
