d82829b7777be6d03e1e2e149601b9716d2fe170
ltdthao <thao452004@gmail.com>
2025-01-09 16:05:33 +0700

    deploy

--- server/index.js
+++ server/index.js
@@ -17,7 +17,6 @@ app.get("/", (req, res) => {
   res.send("Server is working");
 });
 
-// app.use("/uploads", express.static("uploads"));
 // importing routes
 import userRoutes from "./routes/user.js";
 import courseRoutes from "./routes/course.js";
@@ -45,19 +44,3 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
   connectDb();
 });
-
-// import { uploadFiles } from "./middlewares/multer.js";
-// import { handleUpload } from "./config/cloudinary.js";
-// app.post("/upload", uploadFiles, async (req, res) => {
-//   try {
-//     const b64 = Buffer.from(req.file.buffer).toString("base64");
-//     let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
-//     const cldRes = await handleUpload(dataURI);
-//     res.json(cldRes);
-//   } catch (error) {
-//     console.log(error);
-//     res.send({
-//       message: error.message,
-//     });
-//   }
-// });
