e42bc5310256a78db014e0a8ea4631c5f57772cf
Phoegras <164360602+Phoegras@users.noreply.github.com>
2025-01-17 02:27:04 +0700

    Update Noti.jsx

--- frontend/src/pages/notification/Noti.jsx
+++ frontend/src/pages/notification/Noti.jsx
@@ -14,7 +14,7 @@ if (!userId) {
   console.error('User is not logged in!');
 }
 
-const socket = io('https://learning-management-system-xep6.onrender.com', {
+const socket = io('https://learning-management-system-xep6.onrender.com:5001', {
   query: { userId }, // Truyền userId thực tế vào query của Socket.IO
 });
 
