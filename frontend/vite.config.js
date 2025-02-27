f8612718a9dea08d8bccd07bd26471cf310f728e
Nhat Tan <nhattan@Nhats-MacBook-Air.local>
2025-02-27 14:18:48 +0700

    I updated code to deploy 37

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,14 @@ export default defineConfig({
   base: "./",
   build: {
     rollupOptions: {
+      input: "index.html",
       output: {
         manualChunks: {
           reactLibs: ['react', 'react-dom'],
           uiLibs: ['@mui/material', '@mui/icons-material'],
+          entryFileNames: "assets/index-[hash].js",
+          chunkFileNames: "assets/[name]-[hash].js",
+          assetFileNames: "assets/[name]-[hash].[ext]",
         },
       },
     },
