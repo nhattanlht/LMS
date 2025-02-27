1b0c48a3b35fec9c896c45f3f26adb9bb46d1cd4
Nhat Tan <nhattan@Nhats-MacBook-Air.local>
2025-02-27 11:37:36 +0700

    I updated code to deploy 19

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Đảm bảo ứng dụng chạy đúng trên Vercel
-})
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
+  }
+});
