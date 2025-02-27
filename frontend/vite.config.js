600a3d90bf46b67b8a191100ec64a163296e18ef
Nhat Tan <nhattan@Nhats-MacBook-Air.local>
2025-02-27 11:42:04 +0700

    I updated code to deploy 20

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "./",  // Fix lỗi đường dẫn khi deploy
   build: {
     outDir: "dist",
+    assetsDir: "assets"
   },
   server: {
-    historyApiFallback: true,
+    historyApiFallback: true, // Fix lỗi khi dùng React Router
   }
 });
