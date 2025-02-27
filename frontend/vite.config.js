12aa6fa61e738ea6e249ce73d30e58ab5a4b673f
Nhat Tan <nhattan@Nhats-MacBook-Air.local>
2025-02-27 12:44:24 +0700

    I updated code to deploy 24

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",  // Fix lỗi đường dẫn khi deploy
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          reactLibs: ['react', 'react-dom'],
+          uiLibs: ['@mui/material', '@mui/icons-material'],
+        },
+      },
+    },
+  },
 });
